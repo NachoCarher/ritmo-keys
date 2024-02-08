9fffa1beb3d4a48c726715207a52dcf36868b1cf
NachoCarvajal <nachoch64@correo.ugr.es>
2024-02-08 23:58:48 +0100

    Create project basic skeleton

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "./components/TargetText/TargetText.jsx";
+import TargetText from "./components/TargetText/TargetText.jsx";
+
+export default function App() {
+  return (
+    <TargetText />
+  )
+}
---
+++ src/components/TargetText/TargetText.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+
+export default function TargetText() {
+    return (
+        <div className="target-text-container">
+            <input type="text" className="target-text-input" placeholder="Esto es un texto de prueba" />
+        </div>
+        
+    )
+}
