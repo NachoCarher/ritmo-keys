57f589490b53052fe9b2e9c8d3dcf577c63ca391
NachoCarvajal <nachoch64@correo.ugr.es>
2024-02-20 17:43:29 +0100

    Se solapa el input con el progreso actual y el objetivo en placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import TargetText from "./components/TargetText/TargetText.jsx";
 
 export default function App() {
   return (
-    <TargetText />
+    <div>
+      <header className="app-header">
+        <h1>ritmokeys</h1>
+      </header>
+      <main>
+        <TargetText />
+      </main>
+    </div>
   )
 }
--- src/components/TargetText/TargetText.jsx
+++ src/components/TargetText/TargetText.jsx
@@ -1,9 +1,38 @@
 /* eslint-disable react/prop-types */
+import mock from "../../mocks/text1.json";
+import { useState } from "react";
 
 export default function TargetText() {
+    const palabraObjetivo = mock.exampleText;
+    const [letrasIngresadas, setLetrasIngresadas] = useState([]);
+
+    const manejarCambioInput = (event) => {
+        const valorInput = event.target.value;
+        const letras = valorInput.split('');
+    
+        const inputConEstilo = letras.map((letra, index) => {
+          const letraObjetivo = palabraObjetivo[index];
+          const esCorrecta = letra === letraObjetivo;
+    
+          return <span key={index} className={esCorrecta ? 'letra-correcta' : 'letra-incorrecta'}>{letra}</span>;
+        });
+    
+        setLetrasIngresadas(inputConEstilo);
+        //console.log(inputConEstilo);
+      };
+
+    // El input debe mantener el foco siempre
+    const manejarBlurInput = (event) => {
+        event.target.focus();
+    }
+
     return (
         <div className="target-text-container">
-            <input type="text" className="target-text-input" placeholder="Esto es un texto de prueba" />
+            {/* <p>{palabraObjetivo}</p> */}
+            <input type="text" className="target-text-input" autoFocus onChange={manejarCambioInput} onBlur={manejarBlurInput} />
+            <p className="word">{letrasIngresadas}
+              <span className="placeholder">{palabraObjetivo}</span>
+            </p>
         </div>
         
     )
