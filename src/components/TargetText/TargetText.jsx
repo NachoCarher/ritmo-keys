2b94bc3c8b163abf91410c1619e5bfa505a733fc
NachoCarvajal <nachoch64@correo.ugr.es>
2024-02-20 18:57:31 +0100

    Se refactoriza el código

--- src/components/TargetText/TargetText.jsx
+++ src/components/TargetText/TargetText.jsx
@@ -1,39 +1,27 @@
 /* eslint-disable react/prop-types */
 import mock from "../../mocks/text1.json";
 import { useState } from "react";
+import TextoIngresado from '../TextoIngresado/TextoIngresado';
 
 export default function TargetText() {
-    const textoObjetivo = mock.exampleText;
-    const [letrasIngresadas, setLetrasIngresadas] = useState([]);
-    const [textoPlaceholder, setTextoPlaceholder] = useState(textoObjetivo);
+  const textoObjetivo = mock.exampleText;
+  const [textoIngresado, setTextoIngresado] = useState('');
 
-    const manejarCambioInput = (event) => {
-        const valorInput = event.target.value;
-        const letras = valorInput.split('');
-    
-        const inputConEstilo = letras.map((letra, index) => {
-          const letraObjetivo = textoObjetivo[index];
-          const esCorrecta = letra === letraObjetivo;
-    
-          return <span key={index} className={esCorrecta ? 'letra-correcta' : 'letra-incorrecta'}>{letra}</span>;
-        });
-    
-        setLetrasIngresadas(inputConEstilo);
-        setTextoPlaceholder(textoObjetivo.slice(letras.length));
-      };
+  const manejarCambioInput = (event) => {
+    setTextoIngresado(event.target.value);
+  };
 
-    // El input debe mantener el foco siempre
-    const manejarBlurInput = (event) => {
-        event.target.focus();
-    }
+  const manejarBlurInput = (event) => {
+    event.target.focus();
+  }
 
-    return (
-        <div className="target-text-container">
-            <input type="text" spellCheck="false" className="target-text-input" autoFocus onChange={manejarCambioInput} onBlur={manejarBlurInput} />
-            <p className="word">{letrasIngresadas}
-              <span className="placeholder">{textoPlaceholder}</span>
-            </p>
-        </div>
-        
-    )
+  return (
+    <div className="target-text-container">
+      <input type="text" spellCheck="false" className="target-text-input" autoFocus onChange={manejarCambioInput} onBlur={manejarBlurInput} />
+      <p className="word">
+        <TextoIngresado textoIngresado={textoIngresado} textoObjetivo={textoObjetivo} />
+        <span className="placeholder">{textoObjetivo.slice(textoIngresado.length)}</span>
+      </p>
+    </div>
+  )
 }
---
+++ src/components/TextoIngresado/TextoIngresado.jsx
@@ -0,0 +1,14 @@
+
+// eslint-disable-next-line react/prop-types
+function Letra({ letra, esCorrecta }) {
+    return <span className={esCorrecta ? 'letra-correcta' : 'letra-incorrecta'}>{letra}</span>;
+}
+
+export default function TextoIngresado({ textoIngresado, textoObjetivo }) {
+    const letras = textoIngresado.split('');
+    return letras.map((letra, index) => {
+        const letraObjetivo = textoObjetivo[index];
+        const esCorrecta = letra === letraObjetivo;
+        return <Letra key={index} letra={letra} esCorrecta={esCorrecta} />
+    });
+}
