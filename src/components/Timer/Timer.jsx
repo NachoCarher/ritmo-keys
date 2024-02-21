b9c612125712a7fe465358396448d9eae4b9323c
Nacho Carvajal Herrera <nacho.carvajal@unit4.com>
2024-02-21 16:30:58 +0100

    Añade funcionalidad al timer y acorta la longitud de los datos mockeados

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+
 export default function Timer() {
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [milliseconds, setMilliseconds] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setMilliseconds(milliseconds => milliseconds + 1);
+        }, 10);
+
+        if (milliseconds === 100) {
+            setSeconds(seconds => seconds + 1);
+            setMilliseconds(0);
+        }
+
+        if (seconds === 60) {
+            setMinutes(minutes => minutes + 1);
+            setSeconds(0);
+        }
+        
+        return () => clearInterval(interval);
+    }, [milliseconds, seconds]);
+
+    const time = `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}:${milliseconds < 10 ? `0${milliseconds}` : milliseconds}`;
+    
+
     return (
-    <div className="timer">00:00:00</div>
+    <div className="timer">{time}</div>
+
     );
 }
